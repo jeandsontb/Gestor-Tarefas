84a4b6d245a3d5068310288e83cc55ce77bacda6
jeandson <jeandsontb@gmail.com>
2022-05-25 20:22:47 -0300

    include material ui

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+
+const basicTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={basicTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
