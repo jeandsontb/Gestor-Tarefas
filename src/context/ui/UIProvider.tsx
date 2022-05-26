32b0dc2f4f16e12000b651dc54cbfac36d60661f
jeandson <jeandsontb@gmail.com>
2022-05-26 08:32:30 -0300

    create context in application

---
+++ src/context/ui/UIProvider.tsx
@@ -0,0 +1,28 @@
+import { FC, useReducer } from "react";
+import { UIContext, UIReducer } from "./";
+
+export interface UIState {
+  sidemenuOpen: boolean;
+}
+
+const UI_INITIAL_STATE: UIState = {
+  sidemenuOpen: false,
+};
+
+interface Props {
+  children: JSX.Element;
+}
+
+export const UIProvider: FC<Props> = ({ children }) => {
+  const [state, dispatch] = useReducer(UIReducer, UI_INITIAL_STATE);
+
+  return (
+    <UIContext.Provider
+      value={{
+        sidemenuOpen: false,
+      }}
+    >
+      {children}
+    </UIContext.Provider>
+  );
+};
