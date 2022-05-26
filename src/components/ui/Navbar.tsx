0b2aef2aa197ade844efb7509341709f55ea5116
jeandson <jeandsontb@gmail.com>
2022-05-26 20:25:28 -0300

    implemented context

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,11 +1,16 @@
+import { useContext } from "react";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
 
+import { UIContext } from "../../context/ui";
+
 export const Navbar = () => {
+  const { openSideMenu } = useContext(UIContext);
+
   return (
     <AppBar position="sticky">
       <Toolbar>
-        <IconButton size="large" edge="start">
+        <IconButton size="large" edge="start" onClick={openSideMenu}>
           <MenuOutlinedIcon />
         </IconButton>
 
--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import {
   Divider,
   Drawer,
@@ -10,12 +11,15 @@ import {
 import InboxOutlinedIcon from "@mui/icons-material/InboxOutlined";
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import { Box } from "@mui/system";
+import { UIContext } from "../../context/ui";
 
 const menuItems: string[] = ["Inbox", "Start", "Send Email", "Draf"];
 
 export const Sidebar = () => {
+  const { sidemenuOpen, closeSideMenu } = useContext(UIContext);
+
   return (
-    <Drawer anchor="left" open={false} onClose={() => console.log("fechou")}>
+    <Drawer anchor="left" open={sidemenuOpen} onClose={closeSideMenu}>
       <Box sx={{ width: 250 }}>
         <Box sx={{ padding: "5px 10px" }}>
           <Typography variant="h4">Menu</Typography>
